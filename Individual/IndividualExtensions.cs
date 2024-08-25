5e8faf41f12b345bef9d16fd57d0fe77c1eb86f4
rusted-coil <rusted.coil@gmail.com>
2024-08-25 16:46:24 +0900

    Extensions整理

--- Individual/IndividualExtensions.cs
+++ Individual/IndividualExtensions.cs
@@ -1,79 +1,21 @@
-﻿using Gen4RngLib.Rng;
-
-namespace Gen4RngLib.Individual
+﻿namespace Gen4RNGLib.Individual
 {
     public static class IndividualExtensions
     {
         /// <summary>
-        /// LcgRngを使用して性格の決定処理を実行します。
-        /// <para> * Rngの状態を進めます。</para>
+        /// 個体の性格Indexを取得します。
         /// </summary>
-        /// <param name="rng">使用するRng</param>
-        /// <param name="gameVersion">ゲームのバージョン</param>
-        /// <param name="synchroNature">シンクロの性格。シンクロを使用しない場合は-1を指定</param>
-        /// <returns>決定した性格</returns>
-        public static int DetermineNature(this ILcgRng rng, GameVersion gameVersion, int synchroNature)
+        public static int GetNature(this Gen4RngLib.Individual.Individual individual)
         {
-            int nature = -1;
-            if (synchroNature >= 0) // シンクロあり
-            {
-                uint random = rng.Next();
-                if (gameVersion == GameVersion.DPt)
-                {
-                    if (((random >> 15) & 1) == 0)
-                    {
-                        nature = synchroNature;
-                    }
-                }
-                else
-                {
-                    if ((random & 1) == 0)
-                    {
-                        nature = synchroNature;
-                    }
-                }
-            }
-            if (nature < 0) // シンクロなしか不発だった場合は通常の性格決定処理
-            {
-                uint random = rng.Next();
-                if (gameVersion == GameVersion.DPt)
-                {
-                    nature = (int)(random / 0xa3e);
-                }
-                else
-                {
-                    nature = (int)(random % 25);
-                }
-            }
-            return nature;
+            return (int)(individual.PID % 25);
         }
 
         /// <summary>
-        /// RNGを使用して個体を生成します。
-        /// <para> * 膨大な回数呼び出すことを想定しているため、ヒープを使用しないために結果格納用のIndividualを直接渡します。</para>
-        /// <para> * 性格決定処理などを含まず、そのまま性格値を決定します。</para>
+        /// 個体が色違いかどうかを取得します。
         /// </summary>
-        /// <param name="rng">使用するRng</param>
-        /// <param name="nature">指定する性格。-1を指定した場合、直ちにPIDを生成します。</param>
-        /// <param name="createdIndividual">結果を格納するためのIndividualインスタンス</param>
-        public static void GenerateIndividual(this ILcgRng rng, int nature, Individual createdIndividual)
-        {
-            uint pid;
-            do
-            {
-                pid = rng.Next();
-                pid |= (rng.Next() << 16);
-            } while (nature >= 0 && pid % 25 != nature);
-
-            createdIndividual.PID = pid;
-            uint ivs = rng.Next();
-            createdIndividual.HpIV = (ushort)(ivs & 0b11111);
-            createdIndividual.AtkIV = (ushort)((ivs >> 5) & 0b11111);
-            createdIndividual.DefIV = (ushort)((ivs >> 10) & 0b11111);
-            ivs = rng.Next();
-            createdIndividual.SpdIV = (ushort)(ivs & 0b11111);
-            createdIndividual.SpAtkIV = (ushort)((ivs >> 5) & 0b11111);
-            createdIndividual.SpDefIV = (ushort)((ivs >> 10) & 0b11111);
+        public static bool IsShiny(this Gen4RngLib.Individual.Individual individual, uint tid, uint sid)
+        { 
+            return (tid ^ sid ^ (individual.PID & 0xffff) ^ ((individual.PID >> 16) & 0xffff)) <= 7;
         }
     }
 }
---
+++ Individual/LcgRngExtensions.cs
@@ -0,0 +1,79 @@
+﻿using Gen4RngLib.Rng;
+
+namespace Gen4RngLib.Individual
+{
+    public static class LcgRngExtensions
+    {
+        /// <summary>
+        /// LcgRngを使用して性格の決定処理を実行します。
+        /// <para> * Rngの状態を進めます。</para>
+        /// </summary>
+        /// <param name="rng">使用するRng</param>
+        /// <param name="gameVersion">ゲームのバージョン</param>
+        /// <param name="synchroNature">シンクロの性格。シンクロを使用しない場合は-1を指定</param>
+        /// <returns>決定した性格</returns>
+        public static int DetermineNature(this ILcgRng rng, GameVersion gameVersion, int synchroNature)
+        {
+            int nature = -1;
+            if (synchroNature >= 0) // シンクロあり
+            {
+                uint random = rng.Next();
+                if (gameVersion == GameVersion.DPt)
+                {
+                    if (((random >> 15) & 1) == 0)
+                    {
+                        nature = synchroNature;
+                    }
+                }
+                else
+                {
+                    if ((random & 1) == 0)
+                    {
+                        nature = synchroNature;
+                    }
+                }
+            }
+            if (nature < 0) // シンクロなしか不発だった場合は通常の性格決定処理
+            {
+                uint random = rng.Next();
+                if (gameVersion == GameVersion.DPt)
+                {
+                    nature = (int)(random / 0xa3e);
+                }
+                else
+                {
+                    nature = (int)(random % 25);
+                }
+            }
+            return nature;
+        }
+
+        /// <summary>
+        /// RNGを使用して個体を生成します。
+        /// <para> * 膨大な回数呼び出すことを想定しているため、ヒープを使用しないために結果格納用のIndividualを直接渡します。</para>
+        /// <para> * 性格決定処理などを含まず、そのまま性格値を決定します。</para>
+        /// </summary>
+        /// <param name="rng">使用するRng</param>
+        /// <param name="nature">指定する性格。-1を指定した場合、直ちにPIDを生成します。</param>
+        /// <param name="createdIndividual">結果を格納するためのIndividualインスタンス</param>
+        public static void GenerateIndividual(this ILcgRng rng, int nature, Individual createdIndividual)
+        {
+            uint pid;
+            do
+            {
+                pid = rng.Next();
+                pid |= (rng.Next() << 16);
+            } while (nature >= 0 && pid % 25 != nature);
+
+            createdIndividual.PID = pid;
+            uint ivs = rng.Next();
+            createdIndividual.HpIV = (ushort)(ivs & 0b11111);
+            createdIndividual.AtkIV = (ushort)((ivs >> 5) & 0b11111);
+            createdIndividual.DefIV = (ushort)((ivs >> 10) & 0b11111);
+            ivs = rng.Next();
+            createdIndividual.SpdIV = (ushort)(ivs & 0b11111);
+            createdIndividual.SpAtkIV = (ushort)((ivs >> 5) & 0b11111);
+            createdIndividual.SpDefIV = (ushort)((ivs >> 10) & 0b11111);
+        }
+    }
+}
