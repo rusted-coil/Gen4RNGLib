6eb05d14b92b9acc343c399d6fcb53a24166a61d
rusted-coil <rusted.coil@gmail.com>
2024-08-28 09:23:44 +0900

    逆進LcgRngを追加

--- RNG/Internal/LcgRng.cs
+++ RNG/Internal/LcgRng.cs
@@ -4,23 +4,19 @@
     {
         public uint Seed { get; set; }
 
-        public LcgRng(uint initialSeed)
+        readonly uint m_Multiplier;
+        readonly uint m_Adder;
+
+        public LcgRng(uint initialSeed, uint multiplier, uint adder)
         {
             Seed = initialSeed;
-        }
-
-        public LcgRng(DateTime dateTime, uint waitFrames)
-        {
-            Seed = 
-                ((uint)((dateTime.Month * dateTime.Day + dateTime.Minute + dateTime.Second) & 0xff) << 24)
-                | ((uint)dateTime.Hour << 16)
-                | ((uint)(dateTime.Year - 2000));
-            Seed += waitFrames;
+            m_Multiplier = multiplier;
+            m_Adder = adder;
         }
 
         public uint Next()
         {
-            Seed = Seed * 0x41C64E6Du + 0x6073;
+            Seed = Seed * m_Multiplier + m_Adder;
             return Seed >> 16;
         }
     }
--- RNG/RNGFactory.cs
+++ RNG/RNGFactory.cs
@@ -10,7 +10,7 @@
         /// </summary>
         public static ILcgRng CreateLcgRng(uint initialSeed)
         {
-            return new Internal.LcgRng(initialSeed);
+            return new Internal.LcgRng(initialSeed, 0x41C64E6Du, 0x6073);
         }
 
         /// <summary>
@@ -18,7 +18,21 @@
         /// </summary>
         public static ILcgRng CreateLcgRng(DateTime dateTime, uint waitFrames)
         {
-            return new Internal.LcgRng(dateTime, waitFrames);
+            uint initialSeed  =
+                ((uint)((dateTime.Month * dateTime.Day + dateTime.Minute + dateTime.Second) & 0xff) << 24)
+                | ((uint)dateTime.Hour << 16)
+                | ((uint)(dateTime.Year - 2000));
+            initialSeed += waitFrames;
+            return CreateLcgRng(initialSeed);
+        }
+
+        /// <summary>
+        /// 初期seedを与えて逆に進むLcgRngを生成します。
+        /// <para> * 逆RNGは、同じ初期seedで初期化した通常のRNGに対し、通常のRNGの乱数列をr[0],r[1]…とするとr[-2],r[-3]…を返します。</para>
+        /// </summary>
+        public static ILcgRng CreateReverseLcgRng(uint initialSeed)
+        {
+            return new Internal.LcgRng(initialSeed, 0xEEB9EB65u, 0xA3561A1u);
         }
     }
 }
